ac7213fe304ed477c9ea99cc8e3e39eac43c66ad
Daniel Patru <daniel.patru@samraksh.com>
2015-04-09 06:05:56 -0400

    Add copy constructor and assignment operator.

--- q.h
+++ q.h
@@ -1,6 +1,6 @@
 #ifndef Q_H_
 #define Q_H_
-
+#include <algorithm> // std::swap
 #include <cassert> // assert
 // link list implementation of a queue.
 
@@ -45,6 +45,51 @@ template < class Data > class Q {
     assert ( this->invariant() ); 
     return d;
   };
+
+  friend void swap ( Q& x, Q& y ) {
+    std::swap(x.oldest, y.oldest); // repeat for all elements
+    std::swap(x.newest, y.newest);
+  };
+    
+  /** Copy constructor */
+  Q (const Q& other) {
+    this->oldest = 0; this->newest = 0;
+    for (Link *l = other.oldest; l; l = l->oldest ) {
+      this->enq ( l->data );
+    }
+  };
+  /** Copy assignment operator */
+  Q& operator= ( Q other ) {
+    this->swap ( *this, other); 
+    return *this;
+  };
+  /** Destructor */
+  ~Q() /* noexcept */ { /* explicitly specified destructors should be
+		       annotated noexcept as best-practice */
+    while ( ! this->empty() ) this->deq();
+  };
+
+  // C++11
+  /* /\** Move constructor *\/ */
+  /* Q (Q&& other) noexcept { /\* noexcept needed to enable optimizations in containers *\/ */
+  /*   this->oldest = other.oldest; */
+  /*   this->newest = other.newest; */
+  /*   other.oldest = 0; other.newest = 0; */
+  /* }; */
+  /* /\** Copy assignment operator *\/ */
+  /* Q& operator= (const Q& other) { */
+  /*   Q tmp(other); // re-use copy-constructor */
+  /*   *this = std::move(tmp); // re-use move-assignment */
+  /*   return *this; */
+  /* }; */
+  /* /\** Move assignment operator *\/ */
+  /* Q& operator= (Q&& other) noexcept { */
+  /*   // simplified move-constructor that also protects against move-to-self. */
+  /*   std::swap(this->oldest, other.oldest); // repeat for all elements */
+  /*   std::swap(this->newest, other.newest); */
+  /*   return *this; */
+  /* }; */
+ 
 };
   
 #endif // Q_H_
--- testq.cpp
+++ testq.cpp
@@ -3,6 +3,9 @@
 int main() {
   Q<int> q;
   q.enq(1);  q.enq(2);  q.enq(3);
+  Q<int> q2(q), q3=q;
   assert( q.deq() == 1 );   assert( q.deq() == 2 );   assert( q.deq() == 3 );
+  assert( q2.deq() == 1 );   assert( q2.deq() == 2 );   assert( q2.deq() == 3 );
+  assert( q3.deq() == 1 );   assert( q3.deq() == 2 );   assert( q3.deq() == 3 );
 
 };
